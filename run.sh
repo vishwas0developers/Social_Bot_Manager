13305b34db8b102f5cf3f16c9f2fba647be1007d
Vishwas Developers <vishwas0developers@gmail.com>
2025-06-17 16:29:54 +0530

    sh

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Activate the virtual environment
-echo "Activating virtual environment..."
-source "$(dirname "$0")/venv/bin/activate"
+if [ ! -d "venv" ]; then
+  echo "Creating virtual environment..."
+  python3 -m venv venv
+fi
 
-# Run Flask app
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Upgrading pip..."
+pip install --upgrade pip
+
 echo "Running Flask app..."
 python app.py
